78a27703b2ad45de3a36f50dacb93c0e2a466b2e
aboru <aborupro@gmail.com>
2019-11-16 20:59:22 +0900

    Update help's route

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,8 +4,14 @@ RSpec.feature "Statics", type: :feature do
 
   let(:base_title) { "Nobolog" }
 
-  scenario "returns correct title" do
+  scenario "returns correct home title" do
     visit root_path
     expect(page).to have_title "#{base_title}"
   end
+
+  scenario "returns correct help title" do
+    visit help_path
+    expect(page).to have_title "使い方 | #{base_title}"
+  end
+
 end
