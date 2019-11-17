8041ecbec173e8e8b3724114d49b5ff1882d2e67
aboru <aborupro@gmail.com>
2019-11-17 17:36:26 +0900

    Change type feature to system

---
+++ spec/system/static_pages_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Statics", type: :system do
+
+  let(:base_title) { "Nobolog" }
+
+  scenario "returns correct home title" do
+    visit root_path
+    expect(page).to have_title "#{base_title}"
+  end
+
+  scenario "returns correct help title" do
+    visit help_path
+    expect(page).to have_title "使い方 | #{base_title}"
+  end
+
+  scenario "returns correct contact title" do
+    visit contact_path
+    expect(page).to have_title "お問い合わせ | #{base_title}"
+  end
+
+end
