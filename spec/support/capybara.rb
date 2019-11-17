e4bf910bf2656d025425bb5647e369871e61a552
aboru <aborupro@gmail.com>
2019-11-17 17:31:06 +0900

    Add system spec config

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,1 +1,10 @@
 Capybara.javascript_driver = :selenium_chromup
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
+end
