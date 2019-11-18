c2dd08699e2718d636247ca0bd30c15d9be4947a
aboru <aborupro@gmail.com>
2019-11-18 23:29:08 +0900

    Add valid and invalid emails' rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe User, type: :model do
     @user = User.new(name: "Example User", email: "user@example.com")
   end
 
-  it "is valid with a name and an email" do
+  it "is valid with a name and email" do
     expect(@user).to be_valid
   end
 
@@ -27,6 +27,28 @@ RSpec.describe User, type: :model do
   it "is invalid with a too long email" do
     @user.email = "a" * 244 + "@example.com"
     expect(@user).to be_invalid
+  end
+
+  it "is valid with valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn]
+    aggregate_failures do
+      valid_addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid  
+      end
+    end
+  end
+
+  it "is invalid with invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+      foo@bar_baz.com foo@bar+baz.com]
+    aggregate_failures do
+      invalid_addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).to be_invalid  
+      end
+    end
   end
 
 
