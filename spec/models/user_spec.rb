fe337478a69eeb37a63a14cd885ccdf320e68306
aboru <aborupro@gmail.com>
2019-11-19 00:08:00 +0900

    Add email valid spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,26 +30,48 @@ RSpec.describe User, type: :model do
   end
 
   it "is valid with valid addresses" do
-    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-      first.last@foo.jp alice+bob@baz.cn]
     aggregate_failures do
-      valid_addresses.each do |valid_address|
-        @user.email = valid_address
-        expect(@user).to be_valid  
-      end
+      @user.email = "user@example.com"
+      expect(@user).to be_valid
+
+      @user.email = "USER@foo.COM"
+      expect(@user).to be_valid
+
+      @user.email = "A_US-ER@foo.bar.org"
+      expect(@user).to be_valid
+
+      @user.email = "first.last@foo.jp"
+      expect(@user).to be_valid
+
+      @user.email = "alice+bob@baz.cn"
+      expect(@user).to be_valid
     end
   end
 
   it "is invalid with invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-      foo@bar_baz.com foo@bar+baz.com]
     aggregate_failures do
-      invalid_addresses.each do |invalid_address|
-        @user.email = invalid_address
-        expect(@user).to be_invalid  
-      end
+      @user.email = "user@example,com"
+      expect(@user).to be_invalid
+
+      @user.email = "user_at_foo.org"
+      expect(@user).to be_invalid
+
+      @user.email = "user.name@example."
+      expect(@user).to be_invalid
+
+      @user.email = "foo@bar_baz.com"
+      expect(@user).to be_invalid
+
+      @user.email = "foo@bar+baz.com"
+      expect(@user).to be_invalid
     end
   end
 
+  it "is unique email addresses" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    expect(duplicate_user).to be_invalid
+  end
 
 end
