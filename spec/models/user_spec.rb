1d02c58f47f41f00953d9c13008d660c452bbb06
aboru <aborupro@gmail.com>
2019-11-18 22:55:02 +0900

    Add name and email presence rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  it "is valid with a name and an email" do
+    expect(@user).to be_valid
+  end
+
+  it "is invalid without a name" do
+    @user.name = "  "
+    expect(@user).to be_invalid
+  end
+
+  it "is invalid without an email" do
+    @user.email = "  "
+    expect(@user).to be_invalid
+  end
+
+  it "is invalid with a too long name" do
+    @user.name = "a" * 51
+    expect(@user).to be_invalid
+  end
+
+  it "is invalid with a too long email" do
+    @user.email = "a" * 244 + "@example.com"
+    expect(@user).to be_invalid
+  end
+
+
 end
