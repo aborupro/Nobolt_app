6eb342fff2ca64ffbd844af6941c7aef59644930
aboru <aborupro@gmail.com>
2019-11-22 23:55:37 +0900

    Add gem rails-i18n

--- spec/system/users_signup_spec.rb
+++ spec/system/users_signup_spec.rb
@@ -3,23 +3,23 @@ require 'rails_helper'
 RSpec.describe "UsersSignups", type: :system do
   it "is invalid because it has no name" do
     visit signup_path
-    fill_in "Name", with: ''
-    fill_in "Email", with: 'user@invalid'
-    fill_in "Password", with: 'foo'
-    fill_in "Confirmation", with: 'bar'
-    click_on 'Create my account'
+    fill_in "名前", with: ''
+    fill_in "メールアドレス", with: 'user@invalid'
+    fill_in "パスワード", with: 'foo'
+    fill_in "パスワード（確認）", with: 'bar'
+    click_on '登録'
     expect(current_path).to eq signup_path
     expect(page).to have_selector '#error_explanation'
-    expect(page).to have_selector 'li', text: "Name can't be blank"
+    expect(page).to have_selector 'li', text: "名前を入力してください"
   end
 
   it "is valid because it fulfils condition of input" do
     visit signup_path
-    fill_in "Name", with: 'Example User'
-    fill_in 'Email', with: 'user@example.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Confirmation', with: 'password'
-    click_on 'Create my account'
+    fill_in "名前", with: 'Example User'
+    fill_in 'メールアドレス', with: 'user@example.com'
+    fill_in 'パスワード', with: 'password'
+    fill_in 'パスワード（確認）', with: 'password'
+    click_on '登録'
     expect(current_path).to eq user_path(1)
     expect(page).not_to have_selector '#error_explanation'
   end
