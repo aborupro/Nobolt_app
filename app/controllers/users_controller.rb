2069e94addbef022afcca49c4b455d9f3d3e311f
aboru <aborupro@gmail.com>
2019-11-22 22:12:02 +0900

    Add redirect_to @user to users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      redirect_to @user
     else
       render 'new'
     end
