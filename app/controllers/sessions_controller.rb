7a67cc9feecf9f55debf6cd6bf6e1dc2be952f33
aboru <aborupro@gmail.com>
2019-12-01 19:11:23 +0900

    Add edit action for activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = "メールアドレスが確認できていません。 "
+        message += "メールを確認して認証リンクをクリックしてください。"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'メールアドレスまたはパスワードが間違っています'
       render 'new'
