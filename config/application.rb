2e74fcb10789a7467aad4be740d2ee8eb5a2dde1
aboru <aborupro@gmail.com>
2019-12-01 14:22:09 +0900

    Add send mail template

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module NobologApp
     #エラーメッセージを日本語化する設定
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe UserMailer, type: :mailer do
+  describe "account_activation" do
+    let(:mail) { UserMailer.account_activation }
+    let!(:user) { FactoryBot.create(:user)}
+
+    it "renders the headers" do
+      user.activation_token = User.new_token
+      mail = UserMailer.account_activation(user)
+      expect(mail.subject).to eq("Nobologよりメールアドレスの確認")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["noreply@example.com"])
+    end
+
+    it "renders the body" do
+      user.activation_token = User.new_token
+      mail = UserMailer.account_activation(user)
+      expect(mail.body.encoded.split(/\r\n/).map{|i| Base64.decode64(i)}.join).to include("Michael Example")
+    end
+  end
+
+  # describe "password_reset" do
+  #   let(:mail) { UserMailer.password_reset }
+
+  #   it "renders the headers" do
+  #     expect(mail.subject).to eq("Password reset")
+  #     expect(mail.to).to eq(["to@example.org"])
+  #     expect(mail.from).to eq(["noreply@example.com"])
+  #   end
+
+  #   it "renders the body" do
+  #     expect(mail.body.encoded.split(/\r\n/).map{|i| Base64.decode64(i)}.join).to include("Hi")
+  #   end
+  # end
+
+end
