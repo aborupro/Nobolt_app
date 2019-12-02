bb33780172e8496590bd4fd88f7e3af5739fdf86
aboru <aborupro@gmail.com>
2019-12-02 22:25:32 +0900

    Add create action for password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,19 @@
 class PasswordResetsController < ApplicationController
+
   def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "パスワード再設定用のメールを送信しました。"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "メールアドレスが見つかりません。"
+      render 'new'
+    end
   end
 
   def edit
