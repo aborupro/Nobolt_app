6769bc69bf0899b884630c4350cfa91257d21b87
aboru <aborupro@gmail.com>
2019-12-07 15:18:19 +0900

    Add micropost model validation

--- spec/factories/microposts.rb
+++ spec/factories/microposts.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :micropost do
-    content "MyText"
-    user nil
+    content { "Lorem ipsum" }
+    association :user
   end
 end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:micropost) { FactoryBot.build(:micropost) }
+  
+  describe "micropost" do
+    it "is valid" do
+      expect(micropost.valid?).to be_truthy
+    end
+  
+    it "has user id" do
+      micropost.user_id = nil
+      expect(micropost.valid?).to be_falsey
+    end
+
+    context "content" do
+      it "should be present" do
+        micropost.content = " "
+        expect(micropost.valid?).to be_falsey
+      end
+
+      it "requires less than 140 characters" do
+        micropost.content = "a" * 141
+        expect(micropost.valid?).to be_falsey
+      end
+    end
+  end
 end
