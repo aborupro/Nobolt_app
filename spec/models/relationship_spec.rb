6dfb0775ef8037d8c87317952fa252eeb468902f
aboru <aborupro@gmail.com>
2019-12-10 21:52:20 +0900

    Add relationship validation spec

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
   
+  let(:user) { FactoryBot.create(:user) }
+  let(:other_user) { FactoryBot.create(:other_user) }
+  let(:relationship) { Relationship.new(follower_id: user.id,
+                                        followed_id: other_user.id) }
+  
+  it "is valid" do
+    expect(relationship.valid?).to be_truthy
+  end
+
+  it "requires a follower_id" do
+    relationship.follower_id = nil
+    expect(relationship.valid?).to be_falsey
+  end
+
+  it "requires a followed_id" do
+    relationship.followed_id = nil
+    expect(relationship.valid?).to be_falsey
+  end
 end
