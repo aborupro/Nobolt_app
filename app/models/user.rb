7e9ee418010150c22686ad3edba9c3879349557f
aboru <aborupro@gmail.com>
2019-12-10 22:04:22 +0900

    Add following method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -77,6 +78,21 @@ class User < ApplicationRecord
   # 試作feedの定義
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -7,17 +7,28 @@ RSpec.describe Relationship, type: :model do
   let(:relationship) { Relationship.new(follower_id: user.id,
                                         followed_id: other_user.id) }
   
-  it "is valid" do
-    expect(relationship.valid?).to be_truthy
+  describe "validation" do
+    it "is valid" do
+      expect(relationship.valid?).to be_truthy
+    end
+  
+    it "requires a follower_id" do
+      relationship.follower_id = nil
+      expect(relationship.valid?).to be_falsey
+    end
+  
+    it "requires a followed_id" do
+      relationship.followed_id = nil
+      expect(relationship.valid?).to be_falsey
+    end
   end
 
-  it "requires a follower_id" do
-    relationship.follower_id = nil
-    expect(relationship.valid?).to be_falsey
+  it "follows and unfollows a user" do
+    expect(user.following?(other_user)).to be_falsey
+    user.follow(other_user)
+    expect(user.following?(other_user)).to be_truthy
+    user.unfollow(other_user)
+    expect(user.following?(other_user)).to be_falsey
   end
-
-  it "requires a followed_id" do
-    relationship.followed_id = nil
-    expect(relationship.valid?).to be_falsey
-  end
+  
 end
