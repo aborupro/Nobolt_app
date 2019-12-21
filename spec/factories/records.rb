a7f4ba7c155fb6258492926bfc996dc2413b3766
aboru <aborupro@gmail.com>
2019-12-21 22:18:03 +0900

    Add record model

---
+++ spec/factories/records.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :record do
+    grade "MyString"
+    strong_point "MyString"
+    problem_id "MyString"
+    picture "MyString"
+    user nil
+    gym nil
+  end
+end
