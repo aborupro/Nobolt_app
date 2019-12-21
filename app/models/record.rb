9f55348ec031166a6eeafe6895ab1e27f0b4c475
aboru <aborupro@gmail.com>
2019-12-21 22:27:54 +0900

    Add record model validate

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,7 @@
 class Record < ApplicationRecord
   belongs_to :user
   belongs_to :gym
+  validates :grade, presence: true
+  validates :problem_id, presence: true
+  mount_uploader :picture, PictureUploader
 end
