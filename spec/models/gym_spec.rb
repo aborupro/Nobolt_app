3bfdd4f0883ede86980dc7beac3c8349a8a00a96
aboru <aborupro@gmail.com>
2019-12-21 21:35:52 +0900

    Add gym name spec

--- spec/models/gym_spec.rb
+++ spec/models/gym_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Gym, type: :model do
-  let!(:gym) { FactoryBot.build(:gym) }
+  let(:gym) { FactoryBot.build(:gym) }
+  let(:other_gym) { FactoryBot.build(:gym) }
 
   describe "name" do
     it "is valid with a name" do
@@ -11,6 +12,13 @@ RSpec.describe Gym, type: :model do
     it "is invalid with a blank name" do
       gym.name = "　"
       expect(gym).to be_invalid
+    end
+
+    it "is invalid with duplicate name" do
+      gym.name = "Nobolog 東京店"
+      other_gym.name = "Nobolog 東京店"
+      gym.save
+      expect(other_gym).to be_invalid
     end
   end
 end
