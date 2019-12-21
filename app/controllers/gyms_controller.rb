3977bd0af1db328db410c42b3e9d1d46fa1b83ea
aboru <aborupro@gmail.com>
2019-12-21 11:05:23 +0900

    Add gym search with google places api

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -10,28 +10,32 @@ class GymsController < ApplicationController
   end
 
   def new
+    if params[:search].blank?
+      File.open("app/assets/data/bouldering_gyms.json") do |file|
+        @gyms = JSON.load(file)["results"]
+        @flag = 'blank'
+      end
+    else
+      @client = GooglePlaces::Client.new( ENV['GOOGLE_API_KEY'] )
+      @gyms = @client.spots_by_query( params[:search] + "ボルダリング", language: 'ja' )
+      @flag = 'present'
+    end
     @gym = Gym.new
-  end
-
-  def list
-    keyword = params[:search]
-    @client = GooglePlaces::Client.new( ENV['GOOGLE_API_KEY'] )
-    @gyms = @client.spots_by_query( keyword )
   end
 
   def create
     @gym = Gym.new(gym_params)
     if @gym.save
       flash[:info] = "#{@gym.name} を保存しました"
-      redirect_to gym_index_path
+      redirect_to gyms_path
     else
       flash[:danger] = "#{@gym.name} を保存できませんでした"
-      render :index
+      render 'new'
     end
   end
 
   def destroy
-    @place.destroy
+    @gym.destroy
 
     respond_to do |format|
       format.html { redirect_to place_index_path, notice: "#{@place.name} の位置情報を削除しました" }
@@ -40,13 +44,13 @@ class GymsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_place
-      @place = Place.find(params[:id])
+    def set_gym
+      @gym = Gym.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def place_params
-      params.require(:place).permit(:name, :latitude, :longitude, :address)
+    def gym_params
+      params.require(:gym).permit(:name, :address)
     end
 
 end
