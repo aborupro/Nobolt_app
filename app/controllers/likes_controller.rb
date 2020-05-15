716157a15d2edc790c4e0dac71311681cee23fdc
aboru <aborupro@gmail.com>
2020-05-15 23:01:02 +0900

    Add request likes spec

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,21 @@
 class LikesController < ApplicationController
-  before_action :set_record
+  before_action :set_record, :logged_in_user
 
   def create
     @like = current_user.likes.create(record_id: params[:record_id])
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path)}
+      format.js
+    end
   end
 
   def destroy
     @like = Like.find_by(record_id: params[:record_id], user_id: current_user.id)
     @like.destroy
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path)}
+      format.js
+    end
   end
 
   private
