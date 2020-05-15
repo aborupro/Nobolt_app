ce318702aaf59e784dc6ed290a0c1349c07afcc5
aboru <aborupro@gmail.com>
2020-05-15 21:24:49 +0900

    Clean code

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,27 +3,11 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.create(record_id: params[:record_id])
-    # @likes = Like.where(record_id: params[:record_id])
-    # @record.reload
-    # respond_to do |format|
-    #   format.html { redirect_back(fallback_location: root_path)}
-    #   format.js
-    # end
-    # redirect_back(fallback_location: root_path)
-    
   end
 
   def destroy
     @like = Like.find_by(record_id: params[:record_id], user_id: current_user.id)
     @like.destroy
-    # @likes = Like.where(record_id: params[:record_id])
-    # @record.reload
-    # respond_to do |format|
-    #   format.html { redirect_back(fallback_location: root_path)}
-    #   format.js
-    # end
-    # redirect_back(fallback_location: root_path)
-    
   end
 
   private
