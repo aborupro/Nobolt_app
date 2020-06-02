107207efa7701bd04d68ed799ebf5ba44d8f3da5
aboru <aborupro@gmail.com>
2020-06-02 20:55:04 +0900

    Add picture to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_132106) do
+ActiveRecord::Schema.define(version: 2020_06_02_114009) do
 
   create_table "grades", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_132106) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.string "picture"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
