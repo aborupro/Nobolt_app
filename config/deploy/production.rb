0b7cf6b330847b02a61a39959f5f8068be83cf34
aboru <aborupro@gmail.com>
2021-05-24 22:32:29 +0900

    update deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,6 @@
 server '18.178.60.111', user: 'nobolt-hara', roles: %w[app db web]
 
 # デプロイするサーバーにsshログインする鍵の情報を記述
-# set :ssh_options, keys: '~/.ssh/Nobolt-key.pem'
-
 set :ssh_options, {
   keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
   forward_agent: true,
